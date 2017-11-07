09923619701ba6348413ee95f75f537c97c9a39e
[Daph] <[daphnelaura_@hotmail.com]>
2017-11-07 11:07:35 +0100

    draw scatterplot houses

--- file_reader.py
+++ file_reader.py
@@ -1,6 +1,7 @@
 import csv
 from houseClass import houses
 from batteryClass import batteries
+from matplotlib import pyplot as plt
 
 def main():
     file_reader("Huizen&Batterijen/wijk1_huizen.csv", "house")
@@ -30,17 +31,26 @@ def file_reader(file_name, class_name):
                 battery.append(batteries(ID, row[0], row[1], row[2]))
                 ID += 1
 
-        for i in range(150):
-            print(house[0].xLocation, house[0].yLocation)
+        #for i in range(150):
+            #print(house[0].xLocation, house[0].yLocation)
 
 #def draw_grid():
         # get coordinates
-        coordinates_h = []
+        coordinates_x = []
+        coordinates_y = []
 
         for i in range(150):
-            coordinates_h.append((house[i].xLocation, house[i].yLocation))
+            coordinates_x.append(int(house[i].xLocation))
+            coordinates_y.append(int(house[i].yLocation))
 
-        print(coordinates_h)
+        # draw scatterplot
+        x = coordinates_x
+        y = coordinates_y
+
+        plt.scatter(x, y, s=50)
+        plt.grid()
+
+        plt.show()
 
 if __name__ == "__main__":
     main()
