2b56a415b80688a6f4b00b888df706b8d4089a75
[igcsvb] <[ida.segondvonbanchet@student.uva.nl]>
2017-11-08 14:42:10 +0100

    Delete onBattery and onHouse.

--- gridClass.py
+++ gridClass.py
@@ -1,10 +1,8 @@
 class gridPoint:
     """ Class for grid segments. """
 
-    def __init__(self, ID, xLocation, yLocation, houseOnPoint, batteryOnPoint):
+    def __init__(self, ID, xLocation, yLocation):
 
         self.ID = ID
         self.xLocation = xLocation
         self.yLocation = yLocation
-        self.houseOnPoint = houseOnPoint
-        self.batteryOnPoint = batteryOnPoint
