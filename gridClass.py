9e09912f28deb7d5378e0af26ce327a9287de7e9
Sam Kortekaas <samkortekaas1@gmail.com>
2017-11-08 13:58:31 +0100

    Add a class for gridPoint.

---
+++ gridClass.py
@@ -0,0 +1,10 @@
+class gridPoint:
+    """ Class for grid segments. """
+
+    def __init__(self, ID, xLocation, yLocation, houseOnPoint, batteryOnPoint):
+
+        self.ID = ID
+        self.xLocation = xLocation
+        self.yLocation = yLocation
+        self.houseOnPoint = houseOnPoint
+        self.batteryOnPoint = batteryOnPoint
