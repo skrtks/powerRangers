6da3f29ad41a891638e822d4a27e85efba35d531
Sam Kortekaas <samkortekaas1@gmail.com>
2017-11-09 15:15:49 +0100

    Added default value for bool connected.

--- houseClass.py
+++ houseClass.py
@@ -2,7 +2,7 @@ class house:
     ''' Define a class for houses, takes ID, x and y coordinates,
         and power output'''
 
-    def __init__(self, ID, xLocation, yLocation, power, connected):
+    def __init__(self, ID, xLocation, yLocation, power, connected=False):
 
         self.ID = ID
         self.xLocation = xLocation
