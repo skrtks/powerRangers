c1b07dbe192fc8748be07560a6bd53acd391d71e
Sam Kortekaas <samkortekaas1@gmail.com>
2017-11-14 14:03:39 +0100

    Add manhattan distance object.

--- houseClass.py
+++ houseClass.py
@@ -2,10 +2,12 @@ class house:
     ''' Define a class for houses, takes ID, x and y coordinates,
         and power output'''
 
-    def __init__(self, ID, xLocation, yLocation, power, connected=False):
+    def __init__(self, ID, xLocation, yLocation, power, connected=False, manhattanDistance=None):
 
         self.ID = ID
         self.xLocation = xLocation
         self.yLocation = yLocation
         self.power = power
         self.connected = connected
+        if manhattanDistance == None:
+            self.manhattanDistance = []
