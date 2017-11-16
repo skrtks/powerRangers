8a49e54b87db6aa8763909d8c2c8ed8be3adee4f
[igcsvb] <[ida.segondvonbanchet@student.uva.nl]>
2017-11-16 12:50:40 +0100

    Mostly wrote pseudocode for A* in test.py, but it isn't complete and correct yet.

--- test.py
+++ test.py
@@ -9,38 +9,60 @@
 # pseudocode: http://web.mit.edu/eranki/www/tutorials/search/
 def aStar(batteries, houses, grid):
 
+# Loop through all batteries and houses.
 for battery in batteries:
     for house in houses:
 
-    #The open and closed sets
+    # The open and closed sets.
     openset = set()
     closedset = set()
-    #Maak een lege lijst voor path
+
+    # Make an empty list for path.
     path = []
-    #Current point is the battery
+
+    # Current point is the battery.
     current = battery
-    #Add the starting point to the open set
+
+    # Add the starting point to the open set.
     openset.add(current)
-    #While the open set is not empty
+
+    # While the open set is not empty.
     while openset:
-        #Find the item in the open set with the lowest G + H score
-        # current is de node/gridpoint met de laagste score op ManDist (dus niet de ManDist zelf)
-        current = min(openset.gridPoints.manhattanDistance)
+        # Find the item in the open set with the lowest F score.
+        current = min(openset)
 
-
-
-        # add current to path
-        path.add(current)
-
-        #If it is the item we want, retrace the path and return it
+        # If the current point is a house which we want to connect, return path.
         if current == house:
             path = []
             return path
-        #Remove the item from the open set
-        openset.remove(current)
-        #Add it to the closed set
-        closedset.add(current)
 
-        # zoek ID van current
-        # bereken children van nieuwe current
-        children(ID.gridpoint)
+        # Remove the item from the open set.
+        openset.remove(current)
+
+        # Add it to the closed set.
+        closedset.add(current)
+        
+        # Add children of current to the open set.
+        openset.add(children(current, grid))
+
+        # Loop through gridPoints of children.
+        for gridPoint in openset:
+            # If gridPoint is already in closedset, remove from openset.
+            if gridPoint in openset == gridPoint in closedset:
+                openset.remove(gridPoint)
+            # Else gridPoint is in openset for the first time.
+            else:
+                # Get xLocation, yLocation and manhattanDistance of gridPoint.
+                # Add it to a temporary list.
+
+        # Get lowest F score of gridPoints in openset.
+
+        # Add it to path.
+        # Empty temopary list.
+
+        # Return path to draw in grid.
+        return path
+
+
+    #Throw an exception if there is no path
+    raise ValueError('No Path Found')
