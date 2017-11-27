057e856b0dabae687cc650d196d7c81a5423935a
[Daph] <[daphnelaura_@hotmail.com]>
2017-11-27 20:52:04 +0100

    fix bugs optimalization_alorithm

--- optimalization_algorithm.py
+++ optimalization_algorithm.py
@@ -8,47 +8,40 @@ from gridClass import *
 from matplotlib import pyplot as plt
 
 def optimalization_algorithm(houses, batteries):
+    houseOrderX = []
+    houseOrderY = []
     bestScore = 10000
-    for x in range(1000):
+    for x in range(2):
         #print(x)
         connecterScore = 0
         shuffledHousesX = []
         shuffledHousesY = []
         random.shuffle(houses)
+
+        # Set values back to begin values
         for house in houses:
             house.connected = False
+
+        for battery in batteries:
+            battery.capacity = 1507
+            battery.connectedHouses = []
+
         for house in houses:
             counter = 0
+            shuffledHousesX.append(house.xLocation)
+            shuffledHousesY.append(house.yLocation)
             while not house.connected and counter <= 15:
-                shuffledHousesX.append(house.xLocation)
-                shuffledHousesY.append(house.yLocation)
                 for battery in batteries:
-                    batteryCapacity = battery.capacity
                     if house.manhattanDistance[battery.ID] == min(house.manhattanDistance):
-                        if batteryCapacity >= house.power and not house.connected:
-                            batteryCapacity -= house.power
+                        if battery.capacity >= house.power and not house.connected:
+                            battery.capacity -= house.power
                             battery.connectedHouses.append(house.ID)
                             house.connected = True
                             connecterScore += house.manhattanDistance[battery.ID]
                             break
                         else:
                             house.manhattanDistance[battery.ID] = 999
-
                     counter += 1
-
-
-# sortedPower = sorted(houses, key=lambda house: house.power, reverse=True)
-# for house in sortedPower:
-#     while not house.connected:
-#         for battery in batteries:
-#             if house.manhattanDistance[battery.ID] == min(house.manhattanDistance):
-#                 if battery.capacity >= house.power:
-#                     battery.capacity -= house.power
-#                     battery.connectedHouses.append(house.ID)
-#                     house.connected = True
-#                     break
-#                 else:
-#                     house.manhattanDistance[battery.ID] = 999
 
         for house in houses:
             if house.connected == False:
@@ -58,11 +51,18 @@ def optimalization_algorithm(houses, batteries):
             houseOrderX = shuffledHousesX
             houseOrderY = shuffledHousesY
             bestScore = connecterScore
-        print(bestScore)
+        print("bestScore : {}".format(bestScore))
 
-    #return [houseOrderX, houseOrderY, bestScore]
+    # Print statements for checking.
+    for battery in batteries:
+        print("battery capacity[{}]: {}".format(battery.ID, battery.capacity))
+
+    for house in houses:
+        if not house.connected:
+            print("unconnected house(s): {}".format(house.ID))
+            print("power supply unconnected house(s): {}".format(house.power))
+
     print(len(houseOrderX), len(houseOrderY))
-
-
 # [x_h,y_h,s] = connecter()
 # print(s)
+#return [houseOrderX, houseOrderY, bestScore]
