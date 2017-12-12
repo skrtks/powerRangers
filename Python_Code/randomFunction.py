fcc09851dbcf0baf0eaaacfc909fead872ba2d5e
[Daph] <[daphnelaura_@hotmail.com]>
2017-12-12 14:31:15 +0100

    commit for pull

--- Python_Code/randomFunction.py
+++ Python_Code/randomFunction.py
@@ -53,16 +53,13 @@ def randomFunction():
 
             for battery in smartGrid.batteries:
                 battery.connectedHouses = connectedTemp[battery.ID]
-<<<<<<< HEAD
+
                 for houseID in battery.connectedHouses:
                     #print("houseIDnew: {}, batteryIDnew: {}".format(smartGrid.houses[houseID].ID, smartGrid.houses[houseID].batteryId))
                     smartGrid.houses[houseID].batteryId = battery.ID
                     #print("houseIDold: {}, batteryIDold: {}".format(smartGrid.houses[houseID].ID, smartGrid.houses[houseID].batteryId))
 
                 # print("battery[{}]: {}".format(battery.ID, battery.connectedHouses))
-
-=======
->>>>>>> 30869ceb97f0d7a595128cf435c86ab4aa8a060f
 
         #print("bestScore : {}".format(bestScore))
         #print("connecterScore : {}".format(connecterScore))
